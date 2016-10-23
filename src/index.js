92a01921e390681e02749cc236958081ce7656d2
Shaun Thium <shaun.thium@gmail.com>
2016-10-23 01:01:15 -0700

    CSS improvements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Wrapper from './wrapper';
-// import App from './App';
-// import './index.css';
+import './index.css';
 
 ReactDOM.render(
   <Wrapper />,
--- src/wrapper.js
+++ src/wrapper.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Tab from './input/tab';
 import ContentWrapper from './content/wrapper';
+import './wrapper.css';
 
 class Wrapper extends Component {
   constructor(props) {
@@ -17,19 +18,22 @@ class Wrapper extends Component {
   render() {
     return (
       <div>
-        <div className='tabs'>
-          <Tab
-            text='Recommendations'
-            active={this.state.activeTab === 0}
-            onClick={this.setActiveTab.bind(this, 0)}
-            />
-          <Tab
-            text='Marketplace'
-            active={this.state.activeTab === 1}
-            onClick={this.setActiveTab.bind(this, 1)}
-            />
+        <h5>trAInsactions</h5>
+        <div className='container'>
+          <div className='tabs'>
+            <Tab
+              text='Recommendations'
+              active={this.state.activeTab === 0}
+              onClick={this.setActiveTab.bind(this, 0)}
+              />
+            <Tab
+              text='Marketplace'
+              active={this.state.activeTab === 1}
+              onClick={this.setActiveTab.bind(this, 1)}
+              />
+          </div>
+          <ContentWrapper tab={this.state.activeTab} />
         </div>
-        <ContentWrapper tab={this.state.activeTab} />
       </div>
     );
   }
